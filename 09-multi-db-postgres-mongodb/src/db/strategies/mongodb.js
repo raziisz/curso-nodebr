aec35a53846ae5bc4ec2449becf3c5006df1e006
Luiz Felipe Carvalho Libertino <raziel_libertino@hotmail.com>
2020-01-08 16:21:28 -0400

    Finalizado crud e testes com postgres init mongod crud

---
+++ 09-multi-db-postgres-mongodb/src/db/strategies/mongodb.js
@@ -0,0 +1,69 @@
+const ICrud = require("./interfaces/interfaceCrud");
+const Mongoose = require("mongoose");
+
+const STATUS = {
+  0:'Desconectado',
+  1:'Conectado',
+  2:'Conectando',
+  3:'Disconenctando'
+}
+
+
+class MongoDB extends ICrud {
+  constructor() {
+    super();
+    this._herois = null
+    this._driver = null
+  }
+  async isConnected() {
+    const state = STATUS[this._driver.readyState]
+    if(state === 'Conectado') return true;
+    if(state !== 'Conectando') return state
+      await new Promise(resolve => setTimeout(resolve,1000))
+
+    return STATUS[this._driver.readyState]
+  }
+
+  defineModel() {
+    const heroiSchema = new Mongoose.Schema({
+      nome: {
+        type: String,
+        required: true
+      },
+      poder: {
+        type: String,
+        required: true
+      },
+      insertedAt: {
+        type: Date,
+        default: new Date()
+      }
+    })
+    this._herois = Mongoose.model('herois', heroiSchema)
+  }
+
+  connect() {
+    Mongoose.connect(
+      "mongodb://localhost:27017/herois",
+      { useNewUrlParser: true },
+      function(error) {
+        if (!error) return;
+        console.log("Falha na conexao", error);
+      }
+    );
+    const connection = Mongoose.connection;
+    this._driver = connection
+    connection.once('open', () => console.log('Database rodando!!'))
+
+  }
+
+  async create(item) {
+    const resultCadastrar = await model.create({
+    nome: 'Batman',
+    poder: 'Dinheiro'
+  })
+  console.log('result cadastrar', resultCadastrar)
+  }
+}
+
+module.exports = MongoDB;
